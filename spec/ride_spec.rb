e93e4cf24a524cb08955ca16e504c333fec55f50
Kaina Cockett <kainacockett@gmail.com>
2023-06-13 09:45:19 -0600

    test: Ride #initialize

---
+++ spec/ride_spec.rb
@@ -0,0 +1,16 @@
+require 'rspec'
+#require '.lib/visitor'
+#require './lib/ride'
+
+describe 'Ride' do
+  describe '#initialize' do
+    it 'should initialize' do
+      ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+      expect(ride1.name).to eq("Carousel")
+      expect(ride1.min_height).to eq(24)
+      expect(ride1.admission_fee).to eq(1)
+      expect(ride1.excitement).to eq(:gentle)
+      expect(ride1.total_revenue).to eq(0)
+    end
+  end
+end
