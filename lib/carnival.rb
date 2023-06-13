25eeeb4d919b8145b8c51526bc8271b23acf0cde
Kaina Cockett <kainacockett@gmail.com>
2023-06-13 10:47:17 -0600

    feat: Carnival #initialize

---
+++ lib/carnival.rb
@@ -0,0 +1,9 @@
+class Carnival
+
+  attr_reader :duration, :rides
+
+  def initialize(duration)
+    @duration = duration
+    @rides = []
+  end
+end
--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -1,12 +1,14 @@
 require 'rspec'
 require './lib/visitor'
 require './lib/ride'
+require './lib/carnival'
 
 describe 'Carnival' do
   describe '#initialize' do
     it 'should initialize' do
       carnival1 = Carnival.new(14)
       expect(carnival1).to be_a(Carnival)
+      expect(carnival1.duration).to eq(14)
       expect(carnival1.rides).to eq([])
     end
   end
