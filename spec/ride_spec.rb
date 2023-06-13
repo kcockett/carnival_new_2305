ee30d8e365a090cc1e8273e5251abc6b72981cf9
Kaina Cockett <kainacockett@gmail.com>
2023-06-13 10:08:19 -0600

    test: Refactor Ride #board_rider and #rider_log

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -25,6 +25,9 @@ describe 'Ride' do
       ride1.board_rider(visitor1) 
       
       expect(ride1.rider_log).to eq({visitor1 => 2, visitor2 => 1})
+      expect(visitor1.spending_money).to eq(8)
+      expect(visitor2.spending_money).to eq(4)
+      expect(ride1.total_revenue).to eq(3)
     end
   end
 end
