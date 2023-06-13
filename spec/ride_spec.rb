54289ce71e52f58842417cf08fdc6e96ea06e0e2
Kaina Cockett <kainacockett@gmail.com>
2023-06-13 09:59:28 -0600

    test: Ride #board_rider

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -1,5 +1,5 @@
 require 'rspec'
-#require '.lib/visitor'
+require './lib/visitor'
 require './lib/ride'
 
 describe 'Ride' do
@@ -11,6 +11,20 @@ describe 'Ride' do
       expect(ride1.admission_fee).to eq(1)
       expect(ride1.excitement).to eq(:gentle)
       expect(ride1.total_revenue).to eq(0)
+    end
+  end
+  describe '#board_rider and #rider_log' do
+    it 'should check rider height, take money from visitor, and add to ride revenue' do
+      ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+      visitor1 = Visitor.new('Bruce', 54, '$10')
+      visitor2 = Visitor.new('Tucker', 36, '$5')
+      visitor1.add_preference(:gentle)
+      visitor2.add_preference(:gentle)
+      ride1.board_rider(visitor1)
+      ride1.board_rider(visitor2)
+      ride1.board_rider(visitor1)
+      
+      expect(ride1.rider_log).to eq({visitor1 => 2, visitor2 => 1})
     end
   end
 end
