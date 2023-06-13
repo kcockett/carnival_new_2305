66ccab156bb645a873b1abf397dc7a70d90db7c3
Kaina Cockett <kainacockett@gmail.com>
2023-06-13 11:20:35 -0600

    test: Carnival #most_profitable_ride

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -52,4 +52,32 @@ describe 'Carnival' do
       expect(carnival1.most_popular_ride).to eq(ride1)
     end
   end
+  describe '#most_profitable_ride' do
+    it 'should return the ride with the highest revenue' do
+      carnival1 = Carnival.new(14)
+      ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+      ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+      ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
+      carnival1.add_ride(ride1)
+      carnival1.add_ride(ride2)
+      carnival1.add_ride(ride3)
+      visitor1 = Visitor.new('Bruce', 54, '$10')
+      visitor2 = Visitor.new('Tucker', 36, '$5')
+      visitor3 = Visitor.new('Penny', 64, '$15')
+      visitor1.add_preference(:gentle)
+      visitor2.add_preference(:gentle)
+      visitor3.add_preference(:gentle)
+      visitor2.add_preference(:thrilling)
+      visitor3.add_preference(:thrilling)
+      ride1.board_rider(visitor1)
+      ride1.board_rider(visitor2)
+      ride1.board_rider(visitor3)
+      ride1.board_rider(visitor1)
+      ride2.board_rider(visitor1)
+      ride2.board_rider(visitor2)
+      ride3.board_rider(visitor3)
+
+      expect(carnival1.most_profitable_ride).to eq(ride2)
+    end
+  end
 end
