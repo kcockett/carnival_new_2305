d207a4679d103a693b30062750ef9d4a28963abd
Kaina Cockett <kainacockett@gmail.com>
2023-06-13 10:57:26 -0600

    test: Carnival #add_ride

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -4,12 +4,24 @@ require './lib/ride'
 require './lib/carnival'
 
 describe 'Carnival' do
+  before(:each) do
+    @carnival1 = Carnival.new(14)
+    @ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+    @ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+    @ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
+  end
   describe '#initialize' do
     it 'should initialize' do
-      carnival1 = Carnival.new(14)
-      expect(carnival1).to be_a(Carnival)
-      expect(carnival1.duration).to eq(14)
-      expect(carnival1.rides).to eq([])
+      expect(@carnival1).to be_a(Carnival)
+      expect(@carnival1.duration).to eq(14)
+      expect(@carnival1.rides).to eq([])
+    end
+  end
+  describe '#add_ride' do
+    it 'should add a ride to the Carnival' do
+      expect(@carnival1.rides).to eq([])
+      @carnival1.add_ride(@ride1)
+      expect(@carnival1.rides).to eq([@ride1])
     end
   end
 end
