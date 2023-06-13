91930a91d9716048da4ee580bb51f14b9e29a766
Kaina Cockett <kainacockett@gmail.com>
2023-06-13 10:31:30 -0600

    test-fix: Contrain Ride #board_rider

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -28,6 +28,21 @@ describe 'Ride' do
       expect(visitor1.spending_money).to eq(8)
       expect(visitor2.spending_money).to eq(4)
       expect(ride1.total_revenue).to eq(3)
+
+      visitor3 = Visitor.new('Penny', 64, '$15')
+      ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+      ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
+      expect(visitor2.add_preference(:thrilling)).to eq([:gentle, :thrilling])
+      expect(visitor3.add_preference(:thrilling)).to eq([:thrilling])
+
+      ride3.board_rider(visitor1)
+      ride3.board_rider(visitor2)
+      ride3.board_rider(visitor3)
+      expect(visitor1.spending_money).to eq(8)
+      expect(visitor2.spending_money).to eq(4)
+      expect(visitor3.spending_money).to eq(13)
+      expect(ride3.rider_log).to eq({visitor3 => 1})
+      expect(ride3.total_revenue).to eq(2)
     end
   end
 end
